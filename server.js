dc9966f1adf6f32592892517628fb744bcb56c56
piraspberr <86777995+piraspberr@users.noreply.github.com>
2021-09-25 22:08:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,43 @@
 const express = require('express');
 const next = require("next");
-const dev = process.env.NODE_ENV !== "production";
 
-const app1 = next({ dev });
+const app = express()
+const server = require('http').Server(app);
+const { v4: uuidv4} = require('uuid');
+const io = require('socket.io')(server); 
+const {ExpressPeerServer} = require('peer');
 
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 
-const handle = app1.getRequestHandler();
-app1.prepare().then(()=> {
-    const app = express()
-    const server = require('http').Server(app);
-    const { v4: uuidv4} = require('uuid');
-    const io = require('socket.io')(server); 
-    const {ExpressPeerServer} = require('peer');
-
-    const peerServer = ExpressPeerServer(server, {
-        debug: true
-    });
-    app.use(express.static('public'));
-    app.use('/peerjs', peerServer);
-
-    app.set('view engine', 'ejs');
-
-     
+app.set('view engine', 'ejs');
 
 
-    app.get('/', (req, res) => {
-        res.redirect(`/${uuidv4()}`);
-    });
-
-    app.get('/:room', (req, res) => {
-        res.render('room', { roomId: req.param.room })
-    });
 
 
-    io.on('connection', socket => {
-        socket.on('join-room', (roomId, userId) => {
-            socket.join(roomId);
-            socket.to(roomId).broadcast.emit('user-connected', userId);
-            socket.on('message', message => {
-                io.to(roomId).emit('createMessage', message)
-            })
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.param.room })
+});
+
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message)
         })
     })
-
-    server.listen(process.env.PORT || 3500, (err)=>{
-        if(err) throw err;
-        console.log("Server ready!");
-    });
-
-}).catch((ex)=>{
-    console.error(ex.stack);
-    process.exit(1)
 })
+
+server.listen(process.env.PORT || 3500, (err)=>{
+    if(err) throw err;
+    console.log("Server ready!");
+    });
